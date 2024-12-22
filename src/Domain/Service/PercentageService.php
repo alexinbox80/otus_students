048a1c2272e571df32f06e692aef297f7e528b6b
AlekseySerov <alexinbox80@inbox.ru>
2024-12-23 01:10:40 +0300

    Add repository and service for percentage entity

--- src/Domain/Service/PercentageService.php
+++ src/Domain/Service/PercentageService.php
@@ -2,7 +2,108 @@
 
 namespace App\Domain\Service;
 
+use App\Domain\Entity\Percentage;
+use App\Infrastructure\Repository\PercentageRepository;
+
 class PercentageService
 {
+    public function __construct(
+        private readonly PercentageRepository $percentageRepository
+    )
+    {
+    }
 
+    /**
+     * @param int $percentageId
+     * @return Percentage
+     */
+    public function find(int $percentageId): Percentage
+    {
+        return $this->percentageRepository->find($percentageId);
+    }
+
+    /**
+     * @return Percentage[]
+     */
+    public function findAll(): array
+    {
+        return $this->percentageRepository->findAll();
+    }
+
+    /**
+     * @param float $percent
+     * @return Percentage[]
+     */
+    public function findPercentagesByPercent(float $percent): array
+    {
+        return $this->percentageRepository->findPercentagesByPercent($percent);
+    }
+
+    /**
+     * @param string $description
+     * @return Percentage[]
+     */
+    public function findPercentagesByDescription(string $description): array
+    {
+        return $this->percentageRepository->findsPercentagesByDescriptionWithCriteria($description);
+    }
+
+    /**
+     * @param int $percentageId
+     * @param float $percent
+     * @return Percentage|null
+     */
+    public function updatePercent(int $percentageId, float $percent): ?Percentage
+    {
+        $percentage = $this->percentageRepository->find($percentageId);
+        if (!($percentage instanceof Percentage)) {
+            return null;
+        }
+        $this->percentageRepository->updatePercent($percentage, $percent);
+
+        return $percentage;
+    }
+
+    /**
+     * @param int $percentageId
+     * @param string $description
+     * @return Percentage|null
+     */
+    public function updateDescription(int $percentageId, string $description): ?Percentage
+    {
+        $percentage = $this->percentageRepository->find($percentageId);
+        if (!($percentage instanceof Percentage)) {
+            return null;
+        }
+        $this->percentageRepository->updateDescription($percentage, $description);
+
+        return $percentage;
+    }
+
+    /**
+     * @param float $percent
+     * @param string $description
+     * @return Percentage
+     */
+    public function create(float $percent, string $description): Percentage
+    {
+        $percentage = new Percentage();
+        $percentage->setPercent($percent);
+        $percentage->setDescription($description);
+        $this->percentageRepository->create($percentage);
+
+        return $percentage;
+    }
+
+    /**
+     * @param int $percentageId
+     * @return void
+     */
+    public function removeById(int $percentageId): void
+    {
+        $percentage = $this->percentageRepository->find($percentageId);
+        if ($percentage instanceof Percentage) {
+            $this->percentageRepository->remove($percentage);
+        }
+    }
 }
