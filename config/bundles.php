5922852f72d393d3be4b8cc6c17bf6e4477c4059
AlekseySerov <alexinbox80@inbox.ru>
2025-02-16 00:30:06 +0300

    Add rubbit-mq and supervisor support

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    OldSound\RabbitMqBundle\OldSoundRabbitMqBundle::class => ['all' => true],
 ];
