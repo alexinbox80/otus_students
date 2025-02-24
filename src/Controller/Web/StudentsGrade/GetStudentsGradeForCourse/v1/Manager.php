0dad8e9311dc70e6de8e3c47b28d8364ea5e9874
AlexeySerov <alexinbox80@inbox.ru>
2025-02-24 14:11:48 +0300

    Add students grade for course

--- src/Controller/Web/StudentsGrade/GetStudentsGradeForCourse/v1/Manager.php
+++ src/Controller/Web/StudentsGrade/GetStudentsGradeForCourse/v1/Manager.php
@@ -18,28 +18,29 @@ class Manager
 
     public function getStudentGradeForCourse(): array
     {
-//        $students = $this->studentService->findAll();
-//
-//        $totalGrade = [];
-//        foreach ($students as $student) {
-//            $completedTasks = $this->completedTaskService->findByStudent($student);
-//            foreach ($completedTasks as $completedTask) {
-//                $totalGrade[] = [
-//                    'id' => $student->getId(),
-//                    'userName' => $student->getLastName() . ' ' . $student->getFirstName() . ' ' . $student->getMiddleName(),
-//                    'totalGrade' => $this->studentGradeService->getTotalGradeForLesson($completedTask->getTask()->getLesson(), $student)
-//                ];
-//            }
-//        }
-//
-//        return array_map(
-//            static fn (array $totalGrade) => new StudentDTO(
-//                $totalGrade['id'],
-//                $totalGrade['userName'],
-//                $totalGrade['totalGrade'],
-//            ),
-//            $totalGrade
-//        );
-        return [];
+        $students = $this->studentService->findAll();
+
+        $totalGrade = [];
+        foreach ($students as $student) {
+            $completedTasks = $this->completedTaskService->findByStudent($student);
+            foreach ($completedTasks as $completedTask) {
+                $totalGrade[] = [
+                    'id' => $student->getId(),
+                    'userName' => $student->getLastName() . ' ' . $student->getFirstName() . ' ' . $student->getMiddleName(),
+                    'courseName' => $completedTask->getTask()->getLesson()->getCourse()->getName(),
+                    'totalGrade' => $this->studentGradeService->getTotalGradeForCourse($completedTask->getTask()->getLesson()->getCourse(), $student)
+                ];
+            }
+        }
+
+        return array_map(
+            static fn (array $totalGrade) => new StudentDTO(
+                $totalGrade['id'],
+                $totalGrade['userName'],
+                $totalGrade['courseName'],
+                $totalGrade['totalGrade'],
+            ),
+            $totalGrade
+        );
     }
 }
--- src/Controller/Web/StudentsGrade/GetStudentsGradeForCourse/v1/Output/StudentDTO.php
+++ src/Controller/Web/StudentsGrade/GetStudentsGradeForCourse/v1/Output/StudentDTO.php
@@ -7,6 +7,7 @@ class StudentDTO
     public function __construct(
         public readonly int $id,
         public readonly string $studentName,
+        public readonly string $courseName,
         public readonly float $grade
     ) {
     }
